f47370c9b6402d469d160da7cf98e0650d529676
Артем <https://github.com/crazzzytosser/Java-QA@.git>
2024-06-27 11:23:09 +0300

    OOP

---
+++ Exception/DivideByZero.java
@@ -0,0 +1,14 @@
+package Exception;
+
+public class DivideByZero extends MathExceptions{
+
+
+    /**
+     * Исключение
+     *
+     * @param msg - текстовое сообщение для пользователя
+     */
+    public DivideByZero(String msg) {
+        super(msg);
+    }
+}
